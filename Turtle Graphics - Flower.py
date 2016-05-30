5e628105af0a4b0c00ef08b07b8ee729115a1a98
Thy Khue Ly <thykhuely@gmail.com>
2016-05-30 08:02:18 -0400

    Update Turtle Graphics - Flower.py

--- Turtle Graphics - Flower.py
+++ Turtle Graphics - Flower.py
@@ -2,20 +2,20 @@ import turtle
 
 # Turtle is a graphic module introducing programming to kids
 
-def draw_flower(some_turtle):
+def draw_flower(some_turtle, length, angle):
           for i in range (1, 37):
-                    some_turtle.left(35)
-                    some_turtle.forward(100)
-                    some_turtle.right(35)
-                    some_turtle.forward(100)
-                    some_turtle.right(145)
-                    some_turtle.forward(100)
-                    some_turtle.right(35)
-                    some_turtle.forward(100)
+                    some_turtle.left(angle)
+                    some_turtle.forward(length)
+                    some_turtle.right(angle)
+                    some_turtle.forward(length)
+                    some_turtle.right(180-angle)
+                    some_turtle.forward(length)
+                    some_turtle.right(angle)
+                    some_turtle.forward(length)
                     some_turtle.right(10)
                     
           some_turtle.seth(270)
-          some_turtle.forward(400)
+          some_turtle.forward(length * 4)
 
 
 background = turtle.Screen() # create background
